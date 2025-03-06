07e7eff61e50ab23ee8ee34e0f0a452e277e2214
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-06 22:31:32 +0800

    Added further admin customization via admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -4,11 +4,27 @@ from .models import Ingredient, Recipe, RecipeIngredient
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
 
+    list_display = ('name',)
+    list_filter = ('name',)
+    list_display_links = None
+    list_editable = ('name',)
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
 
+    list_display = ('name',)
+    list_filter = ('name',)
+    list_display_links = None
+    list_editable = ('name',)
+
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+
+    list_display = ('quantity','ingredient__name',)
+    list_filter = ('recipe__name',)
+    search_fields = ['ingredient__name','recipe__name']
+    list_display_links = None
+    list_editable = ('quantity',)
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
