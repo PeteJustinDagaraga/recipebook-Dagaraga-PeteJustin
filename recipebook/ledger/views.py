f54db05422c62aa03cac4a28e69774fba855c489
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-10 11:53:29 +0800

    Created lab3 branch

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Recipe, Ingredient, RecipeIngredient
 
 def recipeList(request):
     recipes = Recipe.objects.all()
