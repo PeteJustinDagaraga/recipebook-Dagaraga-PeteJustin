cd3bd7fbc3879785ecd92e50319974b6896fc6c2
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-20 00:06:01 +0800

    Updated recipeEntry.html, and made both list and detail views logged-in user-viewable only

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def recipeList(request):
     recipes = Recipe.objects.all()
     return render(request, "recipeList.html", {'recipes':recipes})
 
+@login_required
 def recipeEntry(request,num=-1):
     if num in range(1,3):
         recipeingredients = RecipeIngredient.objects.filter(recipe__name="Recipe "+str(num))
