e52761291c0e3aa7c95aa6e7c8c3b43b14d6f089
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-20 00:22:42 +0800

    imported User to make Profile work

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User    
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
@@ -15,6 +16,13 @@ class Ingredient(models.Model):
         unique_together = ['name']
         verbose_name = 'ingredient'
         verbose_name_plural = 'ingredients'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
@@ -39,9 +47,3 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE, related_name = 'recipe')
     recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE, related_name = 'ingredient')
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.CharField(max_length=255)
